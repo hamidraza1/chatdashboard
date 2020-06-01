a967d0ba691f75e1a7d25b65da6616d5c3c5726d
hamidraza1 <hrazaa306@gmail.com>
2020-06-01 21:58:40 +0200

    Side Menu Added

---
+++ src/Container/Container.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import TopMenu from "../Components/TopMenu/TopMenu";
+
+class Container extends Component {
+  render() {
+    return (
+      <div>
+        <TopMenu />
+      </div>
+    );
+  }
+}
+
+export default Container;
